099e865e9c76af8ce44c293429b893b0e8c73edc
Alexandr Platoshin <gamesn0len@yahoo.com>
2022-02-02 14:43:04 +0300

    New hometask 2

---
+++ hometask02/script.js
@@ -0,0 +1,58 @@
+/* let myVar;
+
+myVar = 10;
+console.log(typeof myVar);
+myVar = 'Hello world!';
+console.log(typeof myVar);
+myVar = true;
+console.log(typeof myVar);
+myVar = null;
+console.log(typeof myVar);
+myVar = undefined;
+console.log(typeof myVar);
+myVar = Symbol();
+console.log(typeof myVar);
+
+myVar = {}; /*
+// Числа
+
+// Логические опреаторы || && ! 
+// === проверка на равенство без приведеения типов
+// == с приведением типов
+// Math.sqrt() математические опреации
+// Math.pow(какое число, в какую степень)
+// Math.isInteger(число) проверка, является ли число
+// целым
+
+// Строки
+
+// str.length - длина строки
+// str.toUpperCase() - все символы в верхнем регистре
+// str.toLowerCase() - все символы в нижнем регистре
+// str.CharAt() - возвращает сивол по индексу
+// str[i] - возвращает сивол по индексу i
+// str.substring(6) - возвращает подстроку 6>
+/* str.substring(6,15) - возвращает подстроку от 6 до
+15 символа*/ 
+// str.slice(-8) c конца восемь сиволов
+// str.substr(с какого символа, сколько символов)
+// str.indexOf('строка') - индекс первого сивола в параметре
+// str.replace('что поменять', 'на что поменять')
+/* str.split('разделитель') - разбить строку на массив
+по разделителю */
+
+
+let money = 1000000;
+let income = '50000';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 100000;
+let period = 10;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log((addExpenses.toLowerCase()).split(', '));
+let budgetDay = money/30;
+console.log(budgetDay);
